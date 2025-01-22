c52c8465f67e1764bf8ced65980d5544001f16e1
Thaniga141 <thanigakalanidhi1114@gmail.com>
2025-01-22 21:18:27 +0530

    Update ThanigaProject.java

--- src/ProjectWipro/ThanigaProject.java
+++ src/ProjectWipro/ThanigaProject.java
@@ -4,6 +4,7 @@ public class ThanigaProject {
 
 	public static void main(String[] args) {
 		System.out.println("Wipro Training is going to be held");
+		System.out.println("Wipro Training is going to end");
 
 	}
 
